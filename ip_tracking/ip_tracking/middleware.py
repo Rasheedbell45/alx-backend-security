633b86421f175a729b606dfd4e7d11343d859b31
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-04 18:23:08 +0100

    Update middleware.py

--- ip_tracking/ip_tracking/middleware.py
+++ ip_tracking/ip_tracking/middleware.py
@@ -1,20 +1,23 @@
+from django.http import HttpResponseForbidden
 from django.utils.deprecation import MiddlewareMixin
-from .models import RequestLog
+from .models import RequestLog, BlockedIP
 import logging
 
 logger = logging.getLogger(__name__)
+
 
 class RequestLoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
         ip = self.get_client_ip(request)
         path = request.path
-        timestamp = request.timestamp if hasattr(request, "timestamp") else None
 
-        # Save to DB
-        RequestLog.objects.create(
-            ip_address=ip,
-            path=path
-        )
+        # Check if IP is blocked
+        if BlockedIP.objects.filter(ip_address=ip).exists():
+            logger.warning(f"Blocked request from {ip} to {path}")
+            return HttpResponseForbidden("Your IP is blocked.")
+
+        # Save request to DB
+        RequestLog.objects.create(ip_address=ip, path=path)
 
         # Log to console/file
         logger.info(f"Request from {ip} to {path}")

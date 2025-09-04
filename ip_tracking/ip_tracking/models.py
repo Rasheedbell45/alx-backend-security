d62e99b607ac46e8597abfd0ebbd005c7eb7ea0d
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-04 18:40:03 +0100

    Update models.py

--- ip_tracking/ip_tracking/models.py
+++ ip_tracking/ip_tracking/models.py
@@ -18,3 +18,12 @@ class BlockedIP(models.Model):
 
     def __str__(self):
         return self.ip_address
+
+
+class SuspiciousIP(models.Model):
+    ip_address = models.GenericIPAddressField()
+    reason = models.TextField()
+    detected_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.reason} @ {self.detected_at}"
